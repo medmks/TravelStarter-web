2af052c879ee2bd00c3b565a229b8ab9ba8742d3
Mohamed <looklike882@gmail.com>
2024-07-23 18:17:17 +0100

    random change

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 import AboutComponent from "~/components/about.component";
 import HeroComponent  from "~/components/hero.component";
 import Navbar from "~/components/navbar.component";
+
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
