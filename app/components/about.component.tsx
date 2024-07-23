4caefade1e4484b4f4472758a065cd11c74420b1
Mohamed <looklike882@gmail.com>
2024-07-23 18:00:11 +0100

    add : about component

---
+++ app/components/about.component.tsx
@@ -0,0 +1,12 @@
+
+import SectionWrapper from '~/Hoc'
+
+const About = () => {
+  return (
+    <section className='p-14'>
+          about section 
+    </section>
+  )
+}
+
+export default SectionWrapper({ Children:About,id:"about"})
