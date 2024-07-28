01e3c5d9f30c48f33c6aefdef479cc1f3399b613
Mohamed <looklike882@gmail.com>
2024-07-28 21:24:48 +0100

    add flex to navbar

--- app/components/navbar.component.tsx
+++ app/components/navbar.component.tsx
@@ -1,12 +1,12 @@
 
 const Navbar = () => {
   return (
-    <nav className="absolute z-50 w-full p-4 h-[100px] ">
-          <div className="absolute bottom-1/3 left-5 sm:left-16 sm:bottom-0 w-fit ">
+    <nav className="absolute flex justify-center items-end px-20 z-50 w-full  h-[100px]  ">
+          <div className=" flex  ">
             <h1 className="font-outfit font-normal text-2xl sm:text-3xl  text-white">Travelellah</h1>
           </div>
 
-          <div className="hidden absolute bottom-0 right-24">
+          <div className=" hidden sm:flex flex-row justify-end w-full ">
             <ul className=" flex flex-row list-none gap-9 text-white font-outfitsubtext">
               <li className="underline underline-offset-4">Home</li>
               <li><a href="/">About</a></li>
@@ -15,6 +15,7 @@ const Navbar = () => {
               <li><a href="/">Destination</a> </li>
             </ul>
           </div>
+
           <div className="flex justify-end mt-4 pr-3 right-5 bottom-1/3 lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-10">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
