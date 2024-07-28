5b36fa46e420fe1a36001d36510dce7feab572fc
Mohamed <looklike882@gmail.com>
2024-07-28 21:11:31 +0100

    Rm : Hero from the section wrapper

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import AboutComponent from "~/components/about.component";
-import HeroComponent  from "~/components/hero.component";
+import Hero  from "~/components/hero.component";
 import Navbar from "~/components/navbar.component";
 import ServicesComponent from "~/components/services.component";
 
@@ -15,7 +15,7 @@ export default function Index() {
   return (
     <div className="relative z-1 scroll-smooth">
       <Navbar/>
-      <HeroComponent/>
+      <Hero/>
       <AboutComponent/>
       <ServicesComponent/>
     </div>
