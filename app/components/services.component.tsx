f3875373d5196886054eb4725710ab26741cf7ef
Mohamed <looklike882@gmail.com>
2024-08-10 00:16:51 +0100

    Add new pictures

--- app/components/services.component.tsx
+++ app/components/services.component.tsx
@@ -18,7 +18,7 @@ const Services = () => {
       </div>
 
       <div className=" w-full h-[80vh] mt-12 flex flex-col sm:flex-row  gap-5">
-        <div className="w-full h-full bg-hero-pattern3 rounded-2xl object-cover  bg-no-repeat bg-center size-auto bg-cover flex relative">
+        <div className="w-full h-full bg-service-pattern rounded-2xl object-cover  bg-no-repeat bg-center size-auto bg-cover flex relative">
           <div className="flex flex-col gap-2 p-4 pl-7 justify-end items-start">
             <h3 className="text-white font-outfit font-normal text-4xl leading-tight">
               Comprhensive Travel support
@@ -40,7 +40,7 @@ const Services = () => {
             </div>
           </div>
 
-          <div className="w-full h-full bg-hero-pattern4 rounded-2xl object-cover  bg-no-repeat bg-bottom size-auto bg-cover flex relative">
+          <div className="w-full h-full bg-service-pattern1 rounded-2xl object-cover  bg-no-repeat bg-bottom size-auto bg-cover flex relative">
             <div className="flex flex-col gap-2 p-4 justify-end items-start pl-7">
               <h3 className="text-white font-outfit font-normal text-4xl leading-tight">
                 Comprehensive Travel support
