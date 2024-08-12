f8d60b8a7b7693b4596543524f6cdc8bfa906e70
Mohamed <looklike882@gmail.com>
2024-08-12 12:21:54 +0100

    init the package component

--- app/components/package.component.tsx
+++ app/components/package.component.tsx
@@ -1,7 +1,18 @@
 import SectionWrapper from "~/Hoc";
+import { style } from "~/utilities/style";
 
 const Package = () => {
-  return <div>package</div>;
+  return (
+    <section className="relative flex justify-center items-center flex-col">
+          <h2 className={`${style.SubTitle}`}><span className={`${style.SubTitle}`}>{"//"}</span>OUR PACKAGE</h2>
+
+          <div className="flex flex-col gap-3 mt-6 w-4/5">
+                    <h1 className="text-black font-outfit text-center font-normal text-5xl leading-tight">
+                              Discover our exceptional selection of travel package and destinations {" "}
+                    </h1>
+          </div>
+    </section>
+  );
 };
 
 export default SectionWrapper({ Children: Package, id: "package" });
