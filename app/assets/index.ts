2fe5191e58c28ab3e7bb6ad8f867ffa90ddf16b9
Mohamed <looklike882@gmail.com>
2024-08-15 23:20:00 +0100

    add images

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -6,5 +6,9 @@ import traveloka from "./images/traveloka.jpg";
 import booking from "./images/booking-logo.png";
 import airbnb from "./images/airbnb-logo.png";
 import Opendoor from "./images/Opendoor.png";
+import firstImage from "./images/FirstImage.jpg";
+import secondImage from "./images/secondImage.jpg";
+import thirdImage from "./images/thirdimage.jpg";
 
-export { image1, img2, img3, Batman, traveloka, booking, airbnb, Opendoor };
+
+export { image1, img2, img3, Batman, traveloka, booking, airbnb, Opendoor,firstImage, secondImage, thirdImage  };
